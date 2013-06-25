44c18a5f6086e52177cae025d81e4c36511acd06
Illia Golenko <ilya.golenko@gmail.com>
2013-06-25 16:33:01 +0300

    Made search on the main activity

--- src/com/app/parsjson/activity/GetActivity.java
+++ src/com/app/parsjson/activity/GetActivity.java
@@ -1,5 +1,6 @@
 package com.app.parsjson.activity;
 
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
@@ -7,6 +8,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -25,16 +27,27 @@ public class GetActivity extends Activity {
 	public final static String M_ID = "ID";
 	public final static String NAME = "NAME";
 	public final static String POPULARITY = "POPULARITY";
+	public String query;
 	private LinearLayout framesContainer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_get);
-		setTitle("Movie browser");
 		framesContainer = (LinearLayout) findViewById(R.id.mainLayout);
+		
+		Intent intent = getIntent();
+		if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+			String name = intent.getStringExtra(SearchManager.QUERY);
+			query = "http://private-8a74b-themoviedb.apiary.io/3/search/movie?query=" + URLEncoder.encode(name) + "&api_key=9abbb583ac624dedefae66bfb579e008";
+//			query = "http://private-8a74b-themoviedb.apiary.io/3/movie/popular?api_key=9abbb583ac624dedefae66bfb579e008";
+		} else {
+			query = "http://private-8a74b-themoviedb.apiary.io/3/movie/popular?api_key=9abbb583ac624dedefae66bfb579e008";
+		}
+		
 		BrowseMovies listLoader = new BrowseMovies();
 		listLoader.execute();
+
 	}
 
 	@Override
@@ -42,7 +55,6 @@ public class GetActivity extends Activity {
 		getMenuInflater().inflate(R.menu.get, menu);
 		return true;
 	}
-
 
 	private class BrowseMovies extends
 			AsyncTask<Void, Integer, ArrayList<MovieInfo>> {
@@ -53,10 +65,10 @@ public class GetActivity extends Activity {
 
 			try {
 				Downloader download = new Downloader();
-				JSONObject entries = download.GetJson("http://private-8a74b-themoviedb.apiary.io/3/movie/popular?api_key=9abbb583ac624dedefae66bfb579e008");
+				JSONObject entries = download.GetJson(query);
 				JSONArray results = entries.getJSONArray("results");
-
-				for (int i = 0; i < 10; i++) {
+				int count = results.length() < 10 ? results.length() : 10;
+				for (int i = 0; i < count; i++) {
 					JSONObject filmJSON = results.getJSONObject(i);
 					MovieInfo movie = new MovieInfo();
 					movie.setName(filmJSON.getString("original_title"));
@@ -66,9 +78,10 @@ public class GetActivity extends Activity {
 					String url = "https://d3gtl9l2a4fn1j.cloudfront.net/t/p/w185"
 							+ filmJSON.getString("poster_path");
 					movie.setId(filmJSON.getLong("id"));
-					movie.setBmp(download.getImage(url, movie.getId(), getCacheDir()));
+					movie.setBmp(download.getImage(url, movie.getId(),
+							getCacheDir()));
 					list.add(movie);
-					publishProgress(i, 10);
+					publishProgress(i, count);
 				}
 				return list;
 			} catch (JSONException e) {
@@ -87,17 +100,21 @@ public class GetActivity extends Activity {
 				final float popularity = movie.getPoularity();
 				mv.setOnClickListener(new View.OnClickListener() {
 					public void onClick(View v) {
-						Intent intent = new Intent(GetActivity.this, MovieDetails.class);
+						Intent intent = new Intent(GetActivity.this,
+								MovieDetails.class);
 						intent.putExtra(M_ID, id);
 						intent.putExtra(NAME, name);
-						intent.putExtra(POPULARITY, Math.round(popularity) + "%");
+						intent.putExtra(POPULARITY, Math.round(popularity)
+								+ "%");
 						startActivity(intent);
 					}
 				});
 				framesContainer.addView(mv);
 			}
-			((ProgressBar) findViewById(R.id.progressBar2)).setVisibility(View.GONE);
-			((TextView)findViewById(R.id.Downloading)).setVisibility(View.GONE);
+			((ProgressBar) findViewById(R.id.progressBar2))
+					.setVisibility(View.GONE);
+			((TextView) findViewById(R.id.Downloading))
+					.setVisibility(View.GONE);
 		}
 
 		@Override
