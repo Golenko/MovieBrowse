6d4a5d9642194199d41bec2f2787d1faf8bd068a
Illia Golenko <ilya.golenko@gmail.com>
2013-07-05 12:49:03 +0300

    Refactoring to ListView in main activity done.

--- src/com/app/parsjson/activity/MovieList.java
+++ src/com/app/parsjson/activity/MovieList.java
@@ -1,93 +1,142 @@
 package com.app.parsjson.activity;
 
+import java.util.ArrayList;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.app.parsjson.MovieInfo;
-import com.app.parsjson.MovieView;
 import com.example.parsjson.R;
 
 public class MovieList extends SettingsActivity {
-	public final static String M_ID = "ID";
-	public final static String NAME = "NAME";
-	public final static String POPULARITY = "POPULARITY";
-	private LinearLayout framesContainer;
-	private TextView tvInfo;
-	private ProgressBar progress;
+    public final static String M_ID = "ID";
+    public final static String NAME = "NAME";
+    public final static String POPULARITY = "POPULARITY";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_get);
+    final String ATTRIBUTE_TEXT_POPULARITY = "popularity";
+    final String ATTRIBUTE_TEXT_RELEASE = "release";
+    final String ATTRIBUTE_TEXT_NAME = "name";
+    final String ATTRIBUTE_PICTURE = "image";
+    final String ATTRIBUTE_RATING = "rating";
+    ArrayList<Map<String, Object>> data;
+    Map<String, Object> m;
 
-		tvInfo = ((TextView) findViewById(R.id.Downloading));
-		progress = (ProgressBar) findViewById(R.id.progressBar2);
-		framesContainer = (LinearLayout) findViewById(R.id.mainLayout);
+    private TextView tvInfo;
+    private ProgressBar progress;
+    private ListView lvMovies;
 
-		BrowseMovies listLoader = new BrowseMovies(getIntent());
-		listLoader.execute();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get);
 
-	}
+        tvInfo = ((TextView) findViewById(R.id.Downloading));
+        progress = (ProgressBar) findViewById(R.id.progressBar2);
+        lvMovies = (ListView) findViewById(R.id.listView1);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		return true;
+        BrowseMovies listLoader = new BrowseMovies(getIntent());
+        listLoader.execute();
 
-	}
+    }
 
-	private class BrowseMovies extends AsyncTask<Void, Void, List<MovieInfo>> {
-		private final Intent intent;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        return true;
 
-		public BrowseMovies(final Intent intent) {
-			this.intent = intent;
+    }
 
-		}
+    private class BrowseMovies extends AsyncTask<Void, Void, List<MovieInfo>> {
+        private final Intent intent;
 
-		@Override
-		protected List<MovieInfo> doInBackground(Void... arg0) {
+        public BrowseMovies(final Intent intent) {
+            this.intent = intent;
 
-			if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-				String name = intent.getStringExtra(SearchManager.QUERY);
-				return service.searchMovie(name);
-			} else {
-				return service.getMovieList();
-			}
-		}
+        }
 
-		@Override
-		protected void onPostExecute(List<MovieInfo> results) {
-			for (MovieInfo movie : results) {
-				MovieView movieView = new MovieView(getApplicationContext(), movie);
+        @Override
+        protected List<MovieInfo> doInBackground(Void... arg0) {
 
-				final long id = movie.getId();
-				final String name = movie.getName();
-				final float popularity = movie.getPoularity();
-				movieView.setOnClickListener(new View.OnClickListener() {
-					public void onClick(View v) {
-						Intent intent = new Intent(MovieList.this,
-								MovieDetails.class);
-						intent.putExtra(M_ID, id);
-						intent.putExtra(NAME, name);
-						intent.putExtra(POPULARITY, Math.round(popularity)
-								+ "%");
-						startActivity(intent);
-					}
-				});
-				framesContainer.addView(movieView);
-			}
-			progress.setVisibility(View.GONE);
-			tvInfo.setVisibility(View.GONE);
-		}
+            if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+                String name = intent.getStringExtra(SearchManager.QUERY);
+                return service.searchMovie(name);
+            } else {
+                return service.getMovieList();
+            }
+        }
 
-	}
+        @Override
+        protected void onPostExecute(List<MovieInfo> result) {
+            final List<MovieInfo> results = result;
+            String[] from = { ATTRIBUTE_TEXT_RELEASE, ATTRIBUTE_TEXT_POPULARITY, ATTRIBUTE_TEXT_NAME,
+                    ATTRIBUTE_PICTURE, ATTRIBUTE_RATING };
+            int[] to = { R.id.releaseVal, R.id.runtimeVal, R.id.movieName, R.id.moviePic, R.id.movieRate };
+
+            data = new ArrayList<Map<String, Object>>();
+            for (MovieInfo movie : results) {
+                m = new HashMap<String, Object>();
+                m.put(ATTRIBUTE_TEXT_RELEASE, movie.getDate());
+                m.put(ATTRIBUTE_TEXT_POPULARITY, Math.round(movie.getPoularity()) + "%");
+                m.put(ATTRIBUTE_TEXT_NAME, movie.getName());
+                m.put(ATTRIBUTE_PICTURE, movie.getBmp());
+                m.put(ATTRIBUTE_RATING, movie.getRating());
+                data.add(m);
+            }
+            SimpleAdapter sAdapter = new SimpleAdapter(getApplicationContext(), data, R.layout.movie_layout, from, to);
+            sAdapter.setViewBinder(new MyViewBinder());
+            lvMovies.setOnItemClickListener(new OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Log.d("rrrrrrrrr", String.valueOf(results.get((int) id).getId()));
+                    Intent intent = new Intent(MovieList.this, MovieDetails.class);
+                    intent.putExtra(M_ID, results.get((int) id).getId());
+                    intent.putExtra(NAME, results.get((int) id).getName());
+                    intent.putExtra(POPULARITY, Math.round(results.get((int) id).getPoularity()) + "%");
+                    startActivity(intent);
+                }
+            });
+
+            lvMovies.setAdapter(sAdapter);
+            progress.setVisibility(View.GONE);
+            tvInfo.setVisibility(View.GONE);
+            lvMovies.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    class MyViewBinder implements SimpleAdapter.ViewBinder {
+
+        @Override
+        public boolean setViewValue(View view, Object data, String textRepresentation) {
+            switch (view.getId()) {
+            case R.id.movieRate:
+                ((RatingBar) view).setRating((Float) data);
+                return true;
+            case R.id.moviePic:
+                ((ImageView) view).setImageBitmap((Bitmap) data);
+                return true;
+            }
+            return false;
+        }
+
+    }
 }
